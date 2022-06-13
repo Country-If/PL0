8784b712356a26d661cab3eda15d0147a9e2c812
Country-If <2450854008@qq.com>
2022-06-13 15:54:25 +0800

    remove unnecessary comments; reformat file

--- pl0.h
+++ pl0.h
@@ -4,9 +4,9 @@
 
 
 typedef enum {
-	false,
-	true
-}bool;
+    false,
+    true
+} bool;
 
 
 #define norw 13
@@ -18,56 +18,47 @@ typedef enum {
 #define cxmax 200
 
 
-//606  450  509
-/**
-* Created by LuoXintong on 2022/2/25
-*/
 enum symbol {
-	nul, ident, number, plus, minus,
-	times, slash, oddsym, eql, neq,
-	lss, leq, gtr, geq, lparen,
-	rparen, comma, semicolon, period, becomes,
-	beginsym, endsym, ifsym, thensym, whilesym,
-	writesym, readsym, dosym, callsym, constsym,
-	varsym, procsym,
+    nul, ident, number, plus, minus,
+    times, slash, oddsym, eql, neq,
+    lss, leq, gtr, geq, lparen,
+    rparen, comma, semicolon, period, becomes,
+    beginsym, endsym, ifsym, thensym, whilesym,
+    writesym, readsym, dosym, callsym, constsym,
+    varsym, procsym,
 };
 #define symnum 32
 
 /*名字表中的类型*/
 
 enum object {
-	constant,
-	variable,
-	procedur,
+    constant,
+    variable,
+    procedur,
 };
 
 /*虚拟机代码*/
 
 enum fct {
-	lit, opr, lod,
-	sto, cal, inte,
-	jmp, jpc,
+    lit, opr, lod,
+    sto, cal, inte,
+    jmp, jpc,
 };
 #define fctnum 8
 
 /*虚拟机代码结构*/
 
-struct instruction
-{
-	enum fct f;				/*虚拟机代码指令*/
-	int	l;					/*引用层与生命层的层次差*/
-	int	a;					/*根据f的不同而不同*/
+struct instruction {
+    enum fct f;                /*虚拟机代码指令*/
+    int l;                    /*引用层与生命层的层次差*/
+    int a;                    /*根据f的不同而不同*/
 };
 
-/**
-  * Created by FengBoChang on 2022/2/25
-  */
 
-
-FILE* fas;              /* 输出名字表 */
-FILE* fa;               /* 输出虚拟机代码 */
-FILE* fa1;              /* 输出源文件及其各行对应的首地址 */
-FILE* fa2;              /* 输出结果 */
+FILE *fas;              /* 输出名字表 */
+FILE *fa;               /* 输出虚拟机代码 */
+FILE *fa1;              /* 输出源文件及其各行对应的首地址 */
+FILE *fa2;              /* 输出结果 */
 bool listswitch;        /* 显示虚拟机代码与否 */
 bool tableswitch;        /* 显示名字表与否 */
 char ch;                /* 获取字符的缓冲区，getch使用 */
@@ -87,57 +78,76 @@ bool declbegsys[symnum];/* 表示声明开始的符号集合 */
 bool statbegsys[symnum];/* 表示语句开始的符号集合 */
 bool facbegsys[symnum]; /* 表示因子开始的符号集合 */
 /* 名字表结构 */
-struct tablestruct
-{
-	char name[al];      /* 名字 */
-	enum object kind;   /* 类型：const，var，array or procedure */
-	int val;            /* 数值，仅const使用 */
-	int level;          /* 所处层，仅const不使用*/
-	int adr;            /* 地址，仅const不使用 */
-	int size;           /* 需要分配的数据区空间，仅procedure使用 */
+struct tablestruct {
+    char name[al];      /* 名字 */
+    enum object kind;   /* 类型：const，var，array or procedure */
+    int val;            /* 数值，仅const使用 */
+    int level;          /* 所处层，仅const不使用*/
+    int adr;            /* 地址，仅const不使用 */
+    int size;           /* 需要分配的数据区空间，仅procedure使用 */
 };
 struct tablestruct table[txmax]; /* 名字表 */
-FILE* fin;
-FILE* fout;
+FILE *fin;
+FILE *fout;
 char fname[al];
 int err;                /* 错误计数器 */
 
-/**
-  * Created by HeRongXuan on 2022/2/26
-  */
 
-  /* 当函数中会发生fatal error 时，返回-1告知调用它的函数，最终退出程序 */
+/* 当函数中会发生fatal error 时，返回-1告知调用它的函数，最终退出程序 */
 #define getsymdo                   if(-1==getsym()) return -1
 #define getchdo                    if(-1==getch()) return -1
-#define testdo(a,b,c)              if(-1==test(a,b,c)) return -1
-#define gendo(a,b,c)               if(-1==gen(a,b,c)) return -1
-#define expressiondo(a,b,c)        if(-1==expression(a,b,c)) return -1
-#define factordo(a,b,c)            if(-1==factor(a,b,c)) return -1
-#define termdo(a,b,c)              if(-1==term(a,b,c)) return -1
-#define conditiondo(a,b,c)         if(-1==condition(a,b,c)) return -1
-#define statementdo(a,b,c)         if(-1==statement(a,b,c)) return -1
-#define constdeclarationdo(a,b,c)  if(-1==constdeclaration(a,b,c)) return -1
-#define vardeclarationdo(a,b,c)    if(-1==vardeclaration(a,b,c)) return -1
+#define testdo(a, b, c)              if(-1==test(a,b,c)) return -1
+#define gendo(a, b, c)               if(-1==gen(a,b,c)) return -1
+#define expressiondo(a, b, c)        if(-1==expression(a,b,c)) return -1
+#define factordo(a, b, c)            if(-1==factor(a,b,c)) return -1
+#define termdo(a, b, c)              if(-1==term(a,b,c)) return -1
+#define conditiondo(a, b, c)         if(-1==condition(a,b,c)) return -1
+#define statementdo(a, b, c)         if(-1==statement(a,b,c)) return -1
+#define constdeclarationdo(a, b, c)  if(-1==constdeclaration(a,b,c)) return -1
+#define vardeclarationdo(a, b, c)    if(-1==vardeclaration(a,b,c)) return -1
+
 void error(int n);
+
 int getsym();
+
 int getch();
+
 void init();
+
 int gen(enum fct x, int y, int z);
-int test(bool* s1, bool* s2, int n);
-int inset(int e, bool* s);
-int addset(bool* sr, bool* s1, bool* s2, int n);
-int subset(bool* sr, bool* s1, bool* s2, int n);
-int mulset(bool* sr, bool* s1, bool* s2, int n);
-int block(int lev, int tx, bool* fsys);
+
+int test(bool *s1, bool *s2, int n);
+
+int inset(int e, bool *s);
+
+int addset(bool *sr, bool *s1, bool *s2, int n);
+
+int subset(bool *sr, bool *s1, bool *s2, int n);
+
+int mulset(bool *sr, bool *s1, bool *s2, int n);
+
+int block(int lev, int tx, bool *fsys);
+
 void interpret();
-int factor(bool* fsys, int* ptx, int lev);
-int term(bool* fsys, int* ptx, int lev);
-int condition(bool* fsys, int* ptx, int lev);
-int expression(bool* fsys, int* ptx, int lev);
-int statement(bool* fsys, int* ptx, int lev);
+
+int factor(bool *fsys, int *ptx, int lev);
+
+int term(bool *fsys, int *ptx, int lev);
+
+int condition(bool *fsys, int *ptx, int lev);
+
+int expression(bool *fsys, int *ptx, int lev);
+
+int statement(bool *fsys, int *ptx, int lev);
+
 void listcode(int cx0);
-int vardeclaration(int* ptx, int lev, int* pdx);
-int constdeclaration(int* ptx, int lev, int* pdx);
-int position(char* idt, int tx);
-void enter(enum object k, int* ptx, int lev, int* pdx);
-int base(int l, int* s, int b);
+
+int vardeclaration(int *ptx, int lev, int *pdx);
+
+int constdeclaration(int *ptx, int lev, int *pdx);
+
+int position(char *idt, int tx);
+
+void enter(enum object k, int *ptx, int lev, int *pdx);
+
+int base(int l, int *s, int b);
